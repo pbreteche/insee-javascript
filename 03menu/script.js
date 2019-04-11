120056f4edc539fef5e0c445cdf03fdaee6e4cd4
Pierre Bretéché <pbreteche@dawan.fr>
2019-04-11 16:46:26 +0200

    gestion de fermeture du menu

--- 03menu/script.js
+++ 03menu/script.js
@@ -6,6 +6,25 @@ document.addEventListener('DOMContentLoaded', function(){
     // document.getElementById => récupérer un élément par son identifiant
     // document.getElementsByName
 
+    const menu = {
+        actuellementOuvert: null,
+        enCoursOuverture: null,
+    };
+
+
+    document.addEventListener('click', function(){
+        if (menu.enCoursOuverture) {
+            menu.enCoursOuverture.classList.add('active');
+        }
+        if (menu.actuellementOuvert) {
+            menu.actuellementOuvert.classList.remove('active');
+        }
+        // le traitement est terminé, le menu en cours d'ouverture
+        // est considéré ouvert
+        menu.actuellementOuvert = menu.enCoursOuverture;
+        menu.enCoursOuverture = null;
+    });
+
     const interrupteursMenu = document.querySelectorAll('nav > ul > li > a');
 
     for (const interrupteur of interrupteursMenu) {
@@ -15,7 +34,10 @@ document.addEventListener('DOMContentLoaded', function(){
             console.log(this);
             // avec addEventListener, this est forcé sur l'élément
             // sur lequel nous avons posé l'écouteur
-            this.nextElementSibling.classList.add('active');
+            if (menu.actuellementOuvert == this.nextElementSibling) {
+                return;
+            }
+            menu.enCoursOuverture = this.nextElementSibling;
         });
     }
 });
