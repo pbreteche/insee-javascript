bd7f12ca8528a79bb65380a2d90b312d77329f4a
Pierre Bretéché <pbreteche@dawan.fr>
2019-04-11 09:36:26 +0200

    panorama types de bases

---
+++ script.js
@@ -0,0 +1,36 @@
+/**
+ * Types de données
+ */
+
+/**
+ * Types primitifs
+ */
+
+// string
+langage = 'javascript'
+
+// number
+quantite = 1.543
+
+// boolean
+estActif = true
+
+// null
+pasDeDonnées = null
+
+// undefined
+console.log(toto) // undefined
+console.log(titi) // reference error: unknow symbol titi
+toto ='coucou'    // javascript «hisse» les déclarations
+
+
+/**
+ * Types réference
+ */
+unTableau = [true, 123, 'toto', ['oui', 'non']];
+unObjet = {
+    nom: 'table',
+    hauteur: 80,
+    longeur: 120,
+}
+uneFonction = function () { return 1; }
