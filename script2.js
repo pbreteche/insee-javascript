5fb933e05ac05350a30709c779407e0f565c00b6
Pierre Bretéché <pbreteche@dawan.fr>
2019-04-11 11:13:30 +0200

    structure de controle

--- script2.js
+++ script2.js
@@ -16,3 +16,74 @@ const variable4; // portée de bloc + réaffectation interdite
  */
 
 123 == "123.0" // true
+
+/**
+ * structures de contrôle
+ */
+
+/**
+ * conditionnelles
+ */
+
+if ('expression bouléenne') {
+    let i= 0; // la variable n'existe que dans le "if"
+}
+else if ('autre expression') {
+
+}
+else {
+
+}
+
+switch(data) {
+    case 'hello':
+        // faire quelque chose
+        break
+    case 'coucou':
+        // faire quelque chose
+        return
+    case 123:
+        // faire quelque chose
+        throw 'ayayaye'
+
+    default:
+}
+
+'use strict';
+let i = 4;
+let j = 1;
+{
+    let i = 5;
+    j += i
+}
+j += i
+console.log(j) // 10
+
+/**
+ * itérative
+ */
+while('expression booléenne') {
+
+}
+
+for(let i=0; collection.length > i; i++) {
+
+}
+
+const obj = { nom: 'Gérard', prenom: 'Bouchard'}
+obj.nom;
+obj['n'+'om'];
+for(const prop in obj) {
+    console.log(obj[prop]);
+} 
+
+const fruits = ['ananas', 'banane', 'cerise'];
+fruits.ville = 'Orléans';
+
+for (const prop in fruits) {
+    console.log(prop)
+}
+
+for (const fruit of fruits) {
+    console.log(fruit)
+}
