f04470d4745a07beec14a7fa498a6d1d8943ec22
Pierre Bretéché <pbreteche@dawan.fr>
2019-04-11 10:09:44 +0200

    déclaration mots clés

---
+++ script2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+/**
+ * déclaration avec mot clé
+ */
+
+variable1; // non authorisée en mode «strict»
+
+var variable2; // portée de fonction
+let variable3; // portée de bloc
+const variable4; // portée de bloc + réaffectation interdite
+
+/**
+ * Opérateurs
+ */
+
+123 == "123.0" // true
